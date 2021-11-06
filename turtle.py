b63456be8c87fe2336cbf3f2594dd13ddd9a16ae
AidenCheng101 <90733893+AidenCheng101@users.noreply.github.com>
2021-11-06 14:25:56 -0400

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,25 @@
+import turtle
+a = turtle.Turtle()
+a.shape("turtle")
+a.color("red")
+
+a.forward(50)
+a.right(90)
+a.forward(50)
+a.right(90)
+a.forward(50)
+a.right(90)
+a.forward(50)
+a.right(90)
+
+
+b = turtle.Turtle()
+b.shape("arrow")
+b.color("blue")
+
+b.right(60)
+b.forward(50)
+b.right(120)
+b.forward(50)
+b.right(120)
+b.forward(50)
